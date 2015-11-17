cb19b0a62bf2fb9d3edf1b6512e749c7e4ac1d18
Aaron Latham-Ilari <az@dev-io.us>
2015-11-18 08:40:10 +1300

    improved tests

--- tests/SMTPMailerTest.php
+++ tests/SMTPMailerTest.php
@@ -63,14 +63,34 @@ class SMTPMailerTest extends SapphireTest
     public function testEmailCustomHeaders()
     {
 
+        $bcc = "abc-silverstripe-mailer-bcc@mailinator.com";
+        $cc = "abc-silverstripe-mailer-cc@mailinator.com";
+        $replyto = "abc-silverstripe-mailer-reply-to@mailinator.com";
+
         $e = new Email();
         $e->To = "abc-silverstripe-mailer@mailinator.com";
         $e->Subject = "Hi there";
         $e->Body = "I just really wanted to email you and say hi.";
-        $e->Cc = "abc-silverstripe-mailer-cc@mailinator.com";
-        $e->Bcc = "abc-silverstripe-mailer-bcc@mailinator.com";
-        $e->ReplyTo = "abc-silverstripe-mailer-reply-to@mailinator.com";
+        $e->Cc = $cc;
+        $e->Bcc = $bcc;
+        $e->ReplyTo = $replyto;
 
+        // get the mailer bound to the Email class
+        $mailer = Email::mailer();
+
+        // check bccs
+        $bccs = $mailer->getBccAddresses();
+        $this->assertEquals(true, in_array($bcc, $bccs);
+
+        // check ccs
+        $ccs = $mailer->getCcAddresses();
+        $this->assertEquals(true, in_array($cc, $ccs);
+
+        // check replytos
+        $replytos = $mailer->getReplyToAddresses();
+        $this->assertEquals(true, in_array($reployto, $reploytos);
+
+        // check send
         $this->assertEquals(true, $e->send());
 
     }
@@ -80,14 +100,34 @@ class SMTPMailerTest extends SapphireTest
     public function testSMTPEmailCustomHeaders()
     {
 
+        $bcc = "abc-silverstripe-mailer-bcc@mailinator.com";
+        $cc = "abc-silverstripe-mailer-cc@mailinator.com";
+        $replyto = "abc-silverstripe-mailer-reply-to@mailinator.com";
+
         $e = new SMTPEmail();
         $e->To = "abc-silverstripe-mailer@mailinator.com";
         $e->Subject = "Hi there";
         $e->Body = "I just really wanted to email you and say hi.";
-        $e->Cc = "abc-silverstripe-mailer-cc@mailinator.com";
-        $e->Bcc = "abc-silverstripe-mailer-bcc@mailinator.com";
-        $e->ReplyTo = "abc-silverstripe-mailer-reply-to@mailinator.com";
+        $e->Cc = $cc;
+        $e->Bcc = $bcc;
+        $e->ReplyTo = $replyto;
 
+        // get the mailer bound to the Email class
+        $mailer = SMTPEmail::mailer();
+
+        // check bccs
+        $bccs = $mailer->getBccAddresses();
+        $this->assertEquals(true, in_array($bcc, $bccs);
+
+        // check ccs
+        $ccs = $mailer->getCcAddresses();
+        $this->assertEquals(true, in_array($cc, $ccs);
+
+        // check replytos
+        $replytos = $mailer->getReplyToAddresses();
+        $this->assertEquals(true, in_array($reployto, $reploytos);
+
+        // check send
         $this->assertEquals(true, $e->send());
 
     }
