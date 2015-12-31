6beac998359bd9d0792a750de4af86e2977c5709
helpfulrobot <chris@silverstripe.com>
2016-01-01 04:52:13 +1300

    Converted to PSR-2

--- code/SMTPEmail.php
+++ code/SMTPEmail.php
@@ -1,12 +1,14 @@
 <?php
 
-class SMTPEmail extends Email {
+class SMTPEmail extends Email
+{
 
     /**
      * makes ReplyTo work like BCC etc
      * @return [type] [description]
      */
-    public function getReplyTo() {
+    public function getReplyTo()
+    {
         $h = $this->customHeaders;
         return !empty($h['Reply-To']) ? $h['Reply-To'] : null;
     }
@@ -15,7 +17,8 @@ class SMTPEmail extends Email {
      * makes ReplyTo work like BCC etc
      * @param [type] $val [description]
      */
-    public function setReplyTo($val) {
+    public function setReplyTo($val)
+    {
         $this->replyTo($val);
         return $this;
     }
@@ -27,13 +30,18 @@ class SMTPEmail extends Email {
      * @param  [type] $mimetype         [description]
      * @return [type]                   [description]
      */
-    public function attachFile($filename, $attachedFilename = null, $mimetype = null) {
+    public function attachFile($filename, $attachedFilename = null, $mimetype = null)
+    {
 
         // generate a name for the file if we aren't supplied one
-        if (!$attachedFilename) $attachedFilename = trim(str_replace(Director::baseFolder(), '', $filename), '/');
+        if (!$attachedFilename) {
+            $attachedFilename = trim(str_replace(Director::baseFolder(), '', $filename), '/');
+        }
 
         // Get the full path
-        if (!file_exists($filename)) $filename = Director::getAbsFile($filename);
+        if (!file_exists($filename)) {
+            $filename = Director::getAbsFile($filename);
+        }
 
         // try to attach the file
         if (file_exists($filename)) {
@@ -52,17 +60,21 @@ class SMTPEmail extends Email {
      * this is mainly a test harness
      * @return [type] [description]
      */
-    public function setupMailer() {
-
+    public function setupMailer()
+    {
         Requirements::clear();
 
         $this->parseVariables(true);
 
-        if(empty($this->from)) $this->from = Email::config()->admin_email;
+        if (empty($this->from)) {
+            $this->from = Email::config()->admin_email;
+        }
 
         $headers = $this->customHeaders;
 
-        if(project()) $headers['X-SilverStripeSite'] = project();
+        if (project()) {
+            $headers['X-SilverStripeSite'] = project();
+        }
 
         $to = $this->to;
         $from = $this->from;
@@ -70,18 +82,26 @@ class SMTPEmail extends Email {
         if ($sendAllTo = $this->config()->send_all_emails_to) {
             $subject .= " [addressed to $to";
             $to = $sendAllTo;
-            if($this->cc) $subject .= ", cc to $this->cc";
-            if($this->bcc) $subject .= ", bcc to $this->bcc";
+            if ($this->cc) {
+                $subject .= ", cc to $this->cc";
+            }
+            if ($this->bcc) {
+                $subject .= ", bcc to $this->bcc";
+            }
             $subject .= ']';
             unset($headers['Cc']);
             unset($headers['Bcc']);
         } else {
-            if($this->cc) $headers['Cc'] = $this->cc;
-            if($this->bcc) $headers['Bcc'] = $this->bcc;
+            if ($this->cc) {
+                $headers['Cc'] = $this->cc;
+            }
+            if ($this->bcc) {
+                $headers['Bcc'] = $this->bcc;
+            }
         }
 
         if ($ccAllTo = $this->config()->cc_all_emails_to) {
-            if(!empty($headers['Cc']) && trim($headers['Cc'])) {
+            if (!empty($headers['Cc']) && trim($headers['Cc'])) {
                 $headers['Cc'] .= ', ' . $ccAllTo;
             } else {
                 $headers['Cc'] = $ccAllTo;
@@ -89,7 +109,7 @@ class SMTPEmail extends Email {
         }
 
         if ($bccAllTo = $this->config()->bcc_all_emails_to) {
-            if(!empty($headers['Bcc']) && trim($headers['Bcc'])) {
+            if (!empty($headers['Bcc']) && trim($headers['Bcc'])) {
                 $headers['Bcc'] .= ', ' . $bccAllTo;
             } else {
                 $headers['Bcc'] = $bccAllTo;
@@ -97,14 +117,14 @@ class SMTPEmail extends Email {
         }
 
         if ($sendAllfrom = $this->config()->send_all_emails_from) {
-            if($from) $subject .= " [from $from]";
+            if ($from) {
+                $subject .= " [from $from]";
+            }
             $from = $sendAllfrom;
         }
 
         Requirements::restore();
 
         return self::mailer()->setupMailer($to, $from, $subject, $this->attachments, $headers);
-
     }
-
 }
