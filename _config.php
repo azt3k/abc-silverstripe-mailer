d6b84668f620c11b995da6ec119c3acca9a4c65a
Aaron Latham-Ilari <az@dev-io.us>
2017-05-10 13:22:33 +1200

    travis updates

--- _config.php
+++ _config.php
@@ -1,5 +1,7 @@
 <?php
 
-Object::useCustomClass('Email', 'SMTPEmail');
+// deprecated with any luck
+// Object::useCustomClass('Email', 'SMTPEmail');
+
 Email::set_mailer(new SmtpMailer);
 SMTPEmail::set_mailer(new SmtpMailer);
